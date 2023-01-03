837ae799bbea90eaf6403dd262ac9fd2bcfabadc
FoxCrush <foxteryr@gmail.com>
2023-01-03 11:12:18 +0200

    refactor ram main view

--- src/routes/rick-and-morty-view/rick-and-morty-main-view.jsx
+++ src/routes/rick-and-morty-view/rick-and-morty-main-view.jsx
@@ -9,37 +9,34 @@ const ramCharRequestInstance = axios.create({
   baseURL: 'https://rickandmortyapi.com/api/',
 });
 const controller = new AbortController();
-//let data = sessionStorage.getItem("key");
+
 export default function RamMainView() {
   const [characters, setAllCharacters] = useState([]);
   const [dataInfo, setDataInfo] = useState({ pages: 1 });
   const [error, setError] = useState(null);
   const [currentPage, setCurrentPage] = useState(sessionStorage.getItem("page"));
+  const ramCharactersRequest = (query = '', cancelSignal = null) => ramCharRequestInstance
+    .get(query, { signal: cancelSignal })
+    .then(({ data }) => {
+      setAllCharacters(data.results);
+      setDataInfo(data.info);
+    }).catch(error => setError(error))
 
   useEffect(() => {
     if (characters.length !== 0) {
       return
     }
-    ramCharRequestInstance
-      .get(`character/?page=${currentPage}`, { signal: controller.signal })
-      .then(({ data }) => {
-        setAllCharacters(data.results);
-        setDataInfo(data.info);
-      }).catch(error => setError(error));
+    const requestString = `character/?page=${currentPage}`
+    ramCharactersRequest(requestString, controller.signal);
     // Decent way to cancel request in case of component
     // unmount before req settled
-    return () => controller.abort;
+    return () => controller.abort
   }, [characters, currentPage]);
 
   const handlePageClick = event => {
     const { selected } = event;
-    ramCharRequestInstance
-      .get(`character/?page=${selected + 1}`)
-      .then(({ data }) => {
-        setAllCharacters(data.results);
-        setDataInfo(data.info);
-      })
-      .catch(error => setError(error));
+    const requestString = `character/?page=${selected + 1}`
+    ramCharactersRequest(requestString);
       setCurrentPage(selected+1)
       sessionStorage.setItem("page", selected+1);
   };

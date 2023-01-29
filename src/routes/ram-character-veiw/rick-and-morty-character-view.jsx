328eb6cbd97a761eaea11605a0cbed7b5ce11f3d
FoxCrush <foxteryr@gmail.com>
2023-01-29 22:48:21 +0200

    ram character view card

--- src/routes/ram-character-veiw/rick-and-morty-character-view.jsx
+++ src/routes/ram-character-veiw/rick-and-morty-character-view.jsx
@@ -1,6 +1,11 @@
 import axios from 'axios';
 import React, { Fragment, useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleButtonVisibility, setLoading } from '../../redux/ramBtnSlice';
+import MutatingLoader from '../../components/loader';
 
 const ramCharRequestInstance = axios.create({
   baseURL: 'https://rickandmortyapi.com/api/character',
@@ -11,6 +16,7 @@ export default function RamCharacterView() {
   const params = useParams();
   const [character, setCharacter] = useState({});
   const [error, setError] = useState(null);
+  const loading = useSelector(state => state.optionVisibilityControl.isLoading);
 
   useEffect(() => {
     ramCharRequestInstance
@@ -18,25 +24,32 @@ export default function RamCharacterView() {
       .then(({ data }) => {
         setCharacter(data);
       })
-      .catch(error => setError(error));
+      .catch(error => setError(error))
+      .finally();
 
     return () => controller.abort;
   }, [params.id]);
 
   const { id, name, image, status, species } = character;
 
-  if (error) {
-    return <h2>{`HTTP Request error message: ${error.message}`}</h2>;
+  if (loading) {
+    return <MutatingLoader />;
   } else {
     return (
       <Fragment>
-        <h3>{name}</h3>
+        <Card style={{ width: '18rem', margin: '24px 24px' }}>
+          <Card.Img variant="top" src={image} style={{ minHeight: 300 }} />
+          <Card.Body>
+            <Card.Title>{name}</Card.Title>
+            <Card.Text>Status: {status}</Card.Text>
+            <Card.Text>Species: {species}</Card.Text>
+            <Card.Text>ID: {id}</Card.Text>
 
-        <img style={{ minHeight: 300 }} src={image} alt={`${name}'s iamge`} />
-        <p>Character ID: {id}</p>
-        <p>Character Status: {status}</p>
-        <p>Character's Species: {species}</p>
-        <Link to={`/rickandmorty`}>Back to List</Link>
+            <Link to={`/rickandmorty`}>
+              <Button variant="primary">Back to List</Button>
+            </Link>
+          </Card.Body>
+        </Card>
       </Fragment>
     );
   }
